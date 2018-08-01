ab47be44cc661c207c4306fe53838dfa4db31839
Armando Assunção <armando.assuncao.93@gmail.com>
2018-08-01 18:29:30 -0300

    Added i18n fallback

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module RailsBoilerplate
     config.load_defaults 5.2
 
     config.i18n.default_locale = :'pt-BR'
+    config.i18n.fallbacks = [:en]
     config.time_zone = 'Brasilia'
 
     config.api_only = true
